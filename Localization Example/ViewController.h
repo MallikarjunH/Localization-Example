7c48d5013a032e55cecd21502d0c4d9dc91b2d47
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-07-15 12:54:58 +0530

    Added code

---
+++ Localization Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Localization Example
+//
+//  Created by Mallikarjun on 09/05/18.
+//  Copyright © 2018 Mallikarjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
